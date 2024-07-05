e9a6dd7904b479b8bb695ae2d734f3990986f51a
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-07-05 13:54:24 -0400

    added user roles

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,29 +1,26 @@
-// types.ts
-
 export interface Doctor {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    practice: string;
-    availability: Date[];
-    userType: 'doctor';
-  }
-  
-  export interface Patient {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    appointments: string[];
-    userType: 'patient';
-  }
-  
-  export type User = Doctor | Patient;
-  
-  export interface AuthContextType {
-    user: User | null;
-    loginUser: (userData: User) => void;
-    logoutUser: () => void;
-  }
-  
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  practice: string;
+  availability: Date[];
+  userType: 'doctor';
+}
+
+export interface Patient {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  appointments: string[];
+  userType: 'patient';
+}
+
+export type User = Doctor | Patient;
+
+export interface AuthContextType {
+  user: User | null;
+  loginUser: (userData: User) => void;
+  logoutUser: () => void;
+}
