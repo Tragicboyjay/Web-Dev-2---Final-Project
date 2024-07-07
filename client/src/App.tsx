557a1306f61d7e62fa2359aa2be6cea0a4292f54
Bassel-alkhalaf <123759979+Bassel-alkhalaf@users.noreply.github.com>
2024-07-07 19:42:35 -0400

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,19 +11,17 @@ import Home from "./pages/Home";
 import ProfilePage from "./pages/PofilePage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import './App.css';
+import "./App.css";
 const router = createBrowserRouter(
   createRoutesFromElements(
- 
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/dashboard" element={<DoctorDashboard />} />
-        <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/sign_in" element={<SignIn />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/make-appointment" element={<AppointmentPage />} />
-      </Route>
-
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/dashboard" element={<DoctorDashboard />} />
+      <Route path="/sign_up" element={<SignUp />} />
+      <Route path="/sign_in" element={<SignIn />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/make-appointment" element={<AppointmentPage />} />
+    </Route>
   )
 );
 function App() {
